9ce843e3f55fa3df1692cf071e0e3e9bde72c403
82109 <82109@DESKTOP-LLH50T4>
2020-03-20 13:54:42 +0900

    Create a menu

---
+++ src/Helloworld.java
@@ -0,0 +1,15 @@
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("*** Room Management System *** ");
+		System.out.println(" 1. Add Room ");
+		System.out.println(" 2. Delete Room ");
+		System.out.println(" 3. Edit Room ");
+		System.out.println(" 4. View Room ");
+		System.out.println(" 5. Exit ");
+		System.out.println(" Select one number between 1-5 ");
+		System.out.println("****************************** ");
+		
+	}
+
+}
